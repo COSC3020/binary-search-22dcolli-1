7aca8e6f21c6e863379728ce822eff90915e44f2
22dcolli <144738226+22dcolli@users.noreply.github.com>
2024-08-27 00:23:56 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,39 @@
 function binarySearch(list, element) {
+
+    let first = 0;
+    let last = list.length - 1;
+    
+
+    while (first <= last)
+    {
+        let mid = Math.floor((first + last) / 2);
+
+        if (list[mid] == element)
+        {
+            for(let i = mid;list[i] == element; i--)
+            {
+                if(list[i] == element)
+                {
+                    return i
+                }
+            }
+        }
+        else
+        {
+            if (element < list[mid])
+            {
+                last = mid - 1;
+            }
+            else
+            {
+                first = mid + 1;
+            }
+        }
+    }
     return -1;
 }
+
+//testList = [1,2,3,4,5,6,7]
+//testElement = 7
+//console.log(testList)
+//console.log(binarySearch(testList,testElement))
